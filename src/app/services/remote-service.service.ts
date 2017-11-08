dda3b56e76a32d6f1eed1615cbb934d5cac51b1f
Robert Collewijn <robert@tooconnect.nl>
2017-11-08 08:50:09 +0100

    Sorting users by username

--- src/app/services/remote-service.service.ts
+++ src/app/services/remote-service.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {DatePipe} from '@angular/common';
 import {HttpClient} from '@angular/common/http';
-import {IAlbum, IPhoto, IUser} from '../model/interfaces';
 
+import {IAlbum, IPhoto, IUser} from '../model/interfaces';
 
 @Injectable()
 export class RemoteServiceService {
@@ -35,15 +35,31 @@ export class RemoteServiceService {
       );
   }
 
+  sortByUserName(order = 'asc') {
+    this.users = this.users.sort(
+      (a: IUser, b: IUser) => {
+        return (order === 'asc' ? 1 : -1) * (a.username > b.username ? 1 : -1);
+      });
+  }
+
   getAllUsers() {
     console.log('Start: ' + this.datePipe.transform(Date.now(), 'yyyy-MM-dd hh:mm:ss'));
     this.http.get<IUser[]>(this.getApiUrl + '/users')
-      .subscribe(data => {
+    // The .subscribe() method accepts 3 callbacks
+      .subscribe(
+        // The 1st callback handles the data emitted by the observable.
+        data => {
           data.map(user => {
             user.albums = [];
             this.users.push(user);
             this.addAllAlbumsByUser(user);
           });
+        },
+        // The 2nd callback handles errors.
+        (err) => console.error(err),
+        // The 3rd callback handles the "complete" event.
+        () => {
+          this.sortByUserName('desc');
         }
       );
   }

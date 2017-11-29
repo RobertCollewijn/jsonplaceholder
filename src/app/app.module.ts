43f3ec714c200bb9c1e062424bc83392dd741dc4
Robert Collewijn <robert@tooconnect.nl>
2017-11-29 09:54:03 +0100

    Router moved to separate module DataTable added to users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {RouterModule, Routes} from '@angular/router';
+
 import {DatePipe} from '@angular/common';
-
-// materialize
-import {MaterialModule} from './app.material';
-
-
 
 import {HomeComponent} from './web/home/home.component';
 import {RemoteServiceService} from 'app/services/remote-service.service';
-import {AlbumComponent} from './web/home/album/album.component';
-import {PhotoComponent} from './web/home/photo/photo.component';
-import {UserComponent} from './web/home/user/user.component';
-import {UserFormComponent} from './web/home/user/user-form.component';
-import {UsersComponent} from './web/home/users/users.component';
+import {RouteModule} from './app.router';
 
-
-
-const appRoutes: Routes = [
-  {
-    path: 'user/:id',
-    component: UserFormComponent
-  },
-  {
-    path: 'home',
-    component: UsersComponent
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {path: '**', component: UsersComponent}
-];
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AlbumComponent,
-    PhotoComponent,
-    UserComponent,
-    UserFormComponent,
-    UsersComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
-    // FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      appRoutes,
-      {enableTracing: false} // <-- debugging purposes only
-    )
+    RouteModule,
   ],
-  exports: [RouterModule],
+  exports: [RouteModule],
   providers: [RemoteServiceService, DatePipe],
   bootstrap: [HomeComponent]
 })
